6b2f302eb26b161df0be0a4fc2537ef013d6886c
fakeyatogod <kakashi@innocent.com>
2019-11-06 23:25:52 +0530

    Add XiaomiParts and KCal Support

--- device.mk
+++ device.mk
@@ -197,6 +197,10 @@ PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service \
     android.hardware.broadcastradio@1.0-impl
 
+# XiaomiParts
+PRODUCT_PACKAGES += \
+   XiaomiParts
+
 # Doze
 PRODUCT_PACKAGES += \
     XiaomiDoze
